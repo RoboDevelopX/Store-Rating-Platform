0125eeeb9e11cd9e8ced79ba6e5d4e19b57726ce
RoboDevelopX <96328721+RoboDevelopX@users.noreply.github.com>
2025-07-12 13:11:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-Object.defineProperty(exports, 'getCallsite', {
-  enumerable: true,
-  get: function () {
-    return _getCallsite.default;
-  }
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-var _getCallsite = _interopRequireDefault(require('./getCallsite'));
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : {default: obj};
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
